20c61d9373ef3b3a54b0621ba1de647f768e295c
Gustavo B Barska <gbarska@yahoo.com>
2019-09-20 21:00:34 -0300

    base files

---
+++ MvcSalesApp.Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System.IO.Compression;
+using System.Security.Authentication.ExtendedProtection;
+using System;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore;
+using MvcSalesApp.Domain;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace MvcSalesApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public AppDbContext(){}
+
+        public DbSet<Customer> Customers { get; set; }
+
+          public static readonly LoggerFactory MyConsoleLoggerFactory
+         = new LoggerFactory(new [] {
+                new ConsoleLoggerProvider((category, level)
+                 => category == DbLoggerCategory.Database.Command.Name 
+                 && level == LogLevel.Information, true )});
+      
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {            
+                 var connString = "Server=localhost;Port=3306;Database=mvcsales;Uid=gbarska;Pwd=password;";
+               
+                 builder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .UseMySql(connString)
+                .EnableSensitiveDataLogging(true);
+            
+        }
+        public override int SaveChanges()
+        {
+        
+            return base.SaveChanges();
+            
+        }
+       
+    }
+}
