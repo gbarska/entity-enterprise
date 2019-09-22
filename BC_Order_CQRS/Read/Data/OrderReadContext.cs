9fb534b844d07fbd85ee5d798d4bcebdc55016b0
Gustavo B Barska <gbarska@yahoo.com>
2019-09-22 17:48:35 -0300

    adding cqrs to orders

---
+++ BC_Order_CQRS/Read/Data/OrderReadContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Order.Write.Domain;
+using Order.Write.Domain.DTOs;
+
+namespace Order.Read.Data
+{
+    public class OrderReadContext: DbContext
+    {
+      public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+        public OrderContext(){}
+      public DbSet<SalesOrder> Orders { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+      modelBuilder.HasDefaultSchema("Order");
+   }
+
+     protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {            
+                 var connString = "Server=localhost;Port=3306;Database=salesrefac;Uid=gbarska;Pwd=password;";
+               
+                 builder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                 .UseMySql(connString)
+                .EnableSensitiveDataLogging(true);
+            
+        }
+        public override int SaveChanges()
+        {
+        
+            return base.SaveChanges();
+            
+        }
+  }
+}
---
+++ BC_Order_CQRS/Write/Data/OrderWriteContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Order.Write.Domain;
+using Order.Write.Domain.DTOs;
+
+namespace Order.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+        public OrderContext(){}
+
+          public DbSet<SalesOrder> Orders { get; set; }
+          public DbSet<LineItem> LineItems { get; set; }
+    
+          public static readonly LoggerFactory MyConsoleLoggerFactory
+         = new LoggerFactory(new [] {
+                new ConsoleLoggerProvider((category, level)
+                 => category == DbLoggerCategory.Database.Command.Name 
+                 && level == LogLevel.Information, true )});
+      
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("Order");
+            modelBuilder.Entity<SalesOrder>().Ignore(s=>s.LineItems);
+
+            modelBuilder.Ignore<Customer>();
+            modelBuilder.Ignore<CartItem>();
+            
+            //value types mapping in EF
+             modelBuilder.Entity<SalesOrder>().OwnsOne(s => s.ShippingAddress).ToTable("ShippingAddresses");
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {            
+                 var connString = "Server=localhost;Port=3306;Database=salesrefac;Uid=gbarska;Pwd=password;";
+               
+                 builder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                 .UseMySql(connString)
+                .EnableSensitiveDataLogging(true);
+            
+        }
+        public override int SaveChanges()
+        {
+        
+            return base.SaveChanges();
+            
+        }
+       
+    }
+}
